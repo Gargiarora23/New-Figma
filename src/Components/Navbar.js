fd6f00c5fdbb9a0b7ba96fd089dcdbcc50c4ba95
Gargiarora23 <you@example.comgargiarora2323@gmail.com>
2024-11-15 12:30:30 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=" bg-gray-900 w-full h-[53px] flex items-center justify-between px-6">
+    <div className="text-gray-600 font-[Rajdhani] text-[19px] font-medium leading-[19.38px]">
+    GPU AI/ML Apps Todays1 Nvidia GPU AI/ML Apps Todays1 Nvidia
+  </div>
+  <div className='font-[Rajdhani] text-[19px] font-medium leading-[19.38px] text-left underline-offset-[from-font] decoration-skip-ink-none  space-x-4 flex flex-col sm:flex-row justify-between items-center p-6'>
+  <div className='font-extrabold'>Under Attack</div>
+  <div>Log In</div>
+  <div>EN</div>
+  
+  </div>
+</div>
+  )
+}
+
+export default Navbar
